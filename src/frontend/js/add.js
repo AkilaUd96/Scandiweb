72b4925b29d5610add41ade03f567ce3c0ac947c
AkilaUd96 <it17134118@my.sliit.lk>
2022-04-25 21:54:48 +0530

    Commited Scandiweb test

---
+++ src/frontend/js/add.js
@@ -0,0 +1,52 @@
+$(document).ready(() => {
+    $('#type').change(function() {
+        switch (this.value) {
+            case '0':
+                $('#attributes').html(`
+                    <label for="size">Size</label>
+                    <input type="number" step="0.01" class="form-control" name="size" required>
+                    <p>
+                       
+                    </p>
+                `);
+                break;
+            case '1':
+                $('#attributes').html(`
+                    <label for="weight">Weight</label>
+                    <input type="number" step="0.01" class="form-control" name="weight" required>
+                    <p>
+                        
+                    </p>
+                `);
+                break;
+
+            case '2':
+                $('#attributes').html(`
+                    <label for="height">Height</label>
+                    <input type="number" step="0.01" class="form-control" name="height" required>
+                    <label for="width">Width</label>
+                    <input type="number" step="0.01" class="form-control" name="width" required>
+                    <label for="length">Length</label>
+                    <input type="number" step="0.01" class="form-control" name="length" required>
+                    <p>
+                       
+                    </p>
+                `);
+                break;
+        }
+    });
+
+    $('form').submit(function(e) 
+    {
+        e.preventDefault();
+
+        let inputs = {};
+        $(this).find(':input').each(function() {
+            inputs[$(this).attr("name")] = $(this).val();
+        });
+
+        $.post('/products/add', inputs, function(data, status) {
+            $('#message').show().removeClass('alert-success alert-danger').addClass(`alert-${data.status}`).html(data.message);
+        });
+    });
+});
